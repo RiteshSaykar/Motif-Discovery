33a5a55ed8df91e1616efce44d9d79c3ec11d754
RiteshSaykar <107860694+RiteshSaykar@users.noreply.github.com>
2024-04-24 18:42:02 +0530

    c

---
+++ SaleScribe/customer.js
@@ -0,0 +1,8 @@
+function toggleHide() {
+  let add = document.getElementById("details");
+  if (add.style.display != "flex") {
+    add.style.display = "flex";
+  } else {
+    add.style.display = "none";
+  }
+}
---
+++ SaleScribe/home.js
@@ -0,0 +1,3 @@
+const move = document.getElementById('btn3');
+
+move1.addEventListener('click', ()=>{window.location.href = 'sign-in-up.html';})
